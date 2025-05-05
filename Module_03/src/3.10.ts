9f7ba90a05f957ec55f6b39aee4e99a955af99c1
Radiat09 <rzsianboyridoy10@gmail.com>
2025-05-06 02:54:54 +0600

    Encapsulation of OOP in TS:M3-3.10

---
+++ Module_03/src/3.10.ts
@@ -0,0 +1,41 @@
+{
+  //*** Encapsulation */
+
+  // Access Modifiers 1.private, 2.readonly, 3.protected
+
+  class BankAccount {
+    readonly id: number;
+    accountName: string;
+    // private _balance: number; //( _ ) convention of writing private property
+
+    protected _balance: number;
+
+    constructor(id: number, accountName: string, balance: number) {
+      this.id = id;
+      this.accountName = accountName;
+      this._balance = balance;
+    }
+
+    addDeposite(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+    getBalance(): number {
+      return this._balance;
+    }
+  }
+
+  class StudentAccount extends BankAccount {
+    constructor(id: number, accountName: string, balance: number) {
+      super(id, accountName, balance);
+    }
+  }
+
+  const goribManuserAccount = new BankAccount(121, "Gorid Ridoy", 200);
+
+  //   goribManuserAccount.balance = 111;
+  goribManuserAccount.addDeposite(100);
+  const myBalance = goribManuserAccount.getBalance();
+
+  console.log(myBalance);
+}
